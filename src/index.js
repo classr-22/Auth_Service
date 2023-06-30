d324e641c2d1dd7233b59b7e7160ecb612a7a671
classr-22 <roundup414@gmail.com>
2023-06-30 22:58:25 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+// const UserRepository = require('./repository/user-repository');
+
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -13,8 +15,11 @@ const prepareAndStartServer = () => {
 
     app.use('/api',apiRoutes);
 
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`server is running at port : ${PORT}`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response); 
     });
 }
 
