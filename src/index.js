7d76b6f54aaff08577ac25ff8e9d9e56deb0c111
classr-22 <roundup414@gmail.com>
2023-07-01 12:51:28 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-//const UserService = require('./services/user-service');
+const db = require('./models/index');
 
 const app = express();
 
@@ -17,7 +17,9 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT,async()=>{
         console.log(`server is running at port : ${PORT}`);
-        
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
         // const service = new UserService();
         // const newToken = service.createToken({email: 'sumit@admin.com', id:1});
         // console.log("new token is",newToken);
@@ -25,7 +27,6 @@ const prepareAndStartServer = () => {
         // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN1bWl0QGFkbWluLmNvbSIsImlkIjoxLCJpYXQiOjE2ODgxNDc5MTgsImV4cCI6MTY4ODE1MTUxOH0.mwEQuqaozU4bBxdC_FLbIL8NXOJSC_KN7xaUEEI4m3Q'
         // const response=service.verifyToken(token);
         // console.log(response);
-
     });
 }
 
